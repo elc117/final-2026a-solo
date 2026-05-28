dece332cd833f37ccdf6f024a7c14a2cb42ea276
Cadu <carloseduardosil.dev@gmail.com>
2026-05-28 10:58:52 -0300

    Adicionei nucleo da gameplay com movimentação basica e uma screen, altera main para acionar a screen

--- core/src/main/java/io/github/cadu/entities/Player.java
+++ core/src/main/java/io/github/cadu/entities/Player.java
@@ -1,5 +1,44 @@
 package io.github.cadu.entities;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class Player {
-    
-}
+
+    private Texture texturePl;
+
+    private float x;
+    private float y;
+
+    private float speed = 300;
+
+    private float width = 320;
+    private float height = 320;
+
+    public Player() {
+
+        texturePl = new Texture("player.png");
+
+        x = 200;
+        y = 170;
+    }
+
+    public void update(float delta) {
+
+        if(Gdx.input.isKeyPressed(Input.Keys.S)) {
+            x -= speed * delta;
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.D)) {
+            x += speed * delta;
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texturePl, x, y, width, height);
+    }
+public void dispose() {
+    texturePl.dispose();
+}}
--- core/src/main/java/io/github/cadu/screens/GameScreen.java
+++ core/src/main/java/io/github/cadu/screens/GameScreen.java
@@ -1,5 +1,55 @@
 package io.github.cadu.screens;
 
-public class GameScreen {
-    
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import io.github.cadu.entities.Player;
+
+public class GameScreen implements Screen {
+
+    private SpriteBatch batch;
+
+    private Texture background;
+
+    private Player player;
+
+    public GameScreen() {
+
+        batch = new SpriteBatch();
+
+        background = new Texture("bg.png");
+
+        player = new Player();
+    }
+
+    @Override
+    public void render(float delta) {
+
+        ScreenUtils.clear(0, 0, 0, 1);
+
+        player.update(delta);
+
+        batch.begin();
+
+        batch.draw(background, 0, 0);
+
+        player.render(batch);
+
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        background.dispose();
+        player.dispose();
+    }
+
+    @Override public void show() {}
+    @Override public void resize(int width, int height) {}
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
 }
