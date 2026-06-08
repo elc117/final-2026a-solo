5a1d8f75fd59bee1ce93862e4601d512d1a4f1e6
Carlos Eduardo <carloseduardosil.dev@gmail.com>
2026-06-08 17:24:43 -0300

    Implementa versão inicial do menu e adiciona novos assets

--- core/src/main/java/io/github/cadu/Main.java
+++ core/src/main/java/io/github/cadu/Main.java
@@ -1,12 +1,12 @@
 package io.github.cadu;
 
 import com.badlogic.gdx.Game;
-import io.github.cadu.screens.GameScreen;
+import io.github.cadu.screens.MainMenuScreen;
 
 public class Main extends Game {
 
     @Override
     public void create() {
-        setScreen(new GameScreen());
+        setScreen(new MainMenuScreen(this)); //agora chama o menu inves de ir direto pro jogo
     }
 }
---
+++ core/src/main/java/io/github/cadu/screens/MainMenuScreen.java
@@ -0,0 +1,63 @@
+package io.github.cadu.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.ScreenUtils;
+import io.github.cadu.Main;
+
+public class MainMenuScreen implements Screen {
+
+    private Main game;
+    private SpriteBatch batch;
+    
+    private Texture backgroundMenu;
+    private Texture buttonStartTexture;
+    private Rectangle buttonStartBounds;
+
+    public MainMenuScreen(Main game) {
+        this.game = game;
+        batch = new SpriteBatch();
+        
+        backgroundMenu = new Texture("menu_bg.png"); 
+        buttonStartTexture = new Texture("start_game.png"); 
+        
+        buttonStartBounds = new Rectangle(440, 400, 400, 160); 
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0, 1);
+
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+            float mouseX = Gdx.input.getX();
+            float mouseY = Gdx.graphics.getHeight() - Gdx.input.getY();
+            
+            if (buttonStartBounds.contains(mouseX, mouseY)) {
+                game.setScreen(new GameScreen()); // inicia o jogo se clicar no botao de start
+                dispose();
+                return;
+            }
+        }
+        batch.begin();
+        batch.draw(backgroundMenu, 0, 0);
+        batch.draw(buttonStartTexture, buttonStartBounds.x, buttonStartBounds.y, buttonStartBounds.width, buttonStartBounds.height);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        backgroundMenu.dispose();
+        buttonStartTexture.dispose();
+    }
+
+    @Override public void show() {}
+    @Override public void resize(int width, int height) {}
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
+}
