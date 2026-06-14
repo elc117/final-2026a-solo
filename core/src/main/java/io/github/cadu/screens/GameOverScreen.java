76a199f48832bb8de1576001e50d243dc6451ba1
Carlos Eduardo <carloseduardosil.dev@gmail.com>
2026-06-14 00:17:53 -0300

    Corrige bug no fullscreen

--- core/src/main/java/io/github/cadu/screens/GameOverScreen.java
+++ core/src/main/java/io/github/cadu/screens/GameOverScreen.java
@@ -7,6 +7,10 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.math.Vector3;
 import io.github.cadu.Main;
 
 public class GameOverScreen implements Screen {
@@ -17,6 +21,9 @@ public class GameOverScreen implements Screen {
     private Texture backgroundMenu;
     private Texture buttonStartTexture;
     private Rectangle buttonStartBounds;
+    
+    private OrthographicCamera camera;
+    private Viewport viewport;
 
     public GameOverScreen(Main game) {
         this.game = game;
@@ -25,26 +32,36 @@ public class GameOverScreen implements Screen {
         backgroundMenu = new Texture("gameoverbg.png"); 
         buttonStartTexture = new Texture("gameoverbtn.png"); 
         buttonStartBounds = new Rectangle(440, 400, 400, 160); 
+        
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(1280, 960, camera);
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0, 0, 0, 1);
 
+        Vector3 mouseMundo = viewport.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-            float mouseX = Gdx.input.getX();
-            float mouseY = Gdx.graphics.getHeight() - Gdx.input.getY();
-            
-            if (buttonStartBounds.contains(mouseX, mouseY)) {
-                game.setScreen(new MainMenuScreen(game)); // volta pra main menu
+            if (buttonStartBounds.contains(mouseMundo.x, mouseMundo.y)) {
+                game.setScreen(new MainMenuScreen(game)); // Volta pro menu principal de forma segura
                 dispose();
                 return;
             }
         }
+        
+        batch.setProjectionMatrix(camera.combined);
+        
         batch.begin();
-        batch.draw(backgroundMenu, 0, 0);
+        batch.draw(backgroundMenu, 0, 0, 1280, 960);
         batch.draw(buttonStartTexture, buttonStartBounds.x, buttonStartBounds.y, buttonStartBounds.width, buttonStartBounds.height);
         batch.end();
+    }
+
+    @Override 
+    public void resize(int width, int height) { 
+        viewport.update(width, height, true); 
     }
 
     @Override
@@ -55,7 +72,6 @@ public class GameOverScreen implements Screen {
     }
 
     @Override public void show() {}
-    @Override public void resize(int width, int height) {}
     @Override public void pause() {}
     @Override public void resume() {}
     @Override public void hide() {}
